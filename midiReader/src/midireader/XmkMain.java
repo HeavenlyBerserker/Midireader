1c24127cb274ca6d8f5c9e6523dbe8032c85d4a2
joelmichelson <joelmichelson@gmail.com>
2016-11-18 01:07:46 -0600

    Skyline 3: The return of the skyline

--- midiReader/src/midireader/XmkMain.java
+++ midiReader/src/midireader/XmkMain.java
@@ -56,13 +56,7 @@ public class XmkMain {
     public static void main(String[] args) throws Exception {
         
         List<String> files = new ArrayList<>();
-<<<<<<< HEAD
         Path dir = Paths.get("input/InputV1/TestFiles");
-=======
-        
-        Path dir = Paths.get("input/InputV1/TestFiles");
-        
->>>>>>> 16f9aaa7a4d260a98cb5d6fab28a946101de6f34
         MelismaReader.getFileNames(files, dir);
         int successes = 0;
         
